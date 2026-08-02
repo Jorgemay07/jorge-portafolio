ab79c51d677bc139539fc2ab02b9a35e20708ab6
Jorge Luis May Espinoza <{email}>
2026-08-02 15:58:32 -0600

    Actualización de contenido de especializacion

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -161,6 +161,48 @@ export const projects = [
     ]
   },
     {
+    slug: "Networking",
+
+    title: "Networking",
+
+    category: "Infraestructura",
+
+    status: "Producción",
+
+    year: "2019 - Actualidad",
+
+    icon: "network",
+
+    description:
+      "Administración de infraestructura de red, incluyendo switches, routers y firewalls, para garantizar conectividad y seguridad en la institución.",
+
+    overview:
+      "Implementación y administración de la infraestructura de red para garantizar conectividad y seguridad en la institución.",
+
+    technologies: [
+      "DHCP",
+      "DNS",
+      "Active Directory",
+      "Switches",
+      "Routers",
+      "Firewalls"
+    ],
+
+    responsibilities: [
+      "Configuración de hosts.",
+      "Alertas.",
+      "Dashboards.",
+      "Monitoreo de servicios.",
+      "Análisis de disponibilidad."
+    ],
+
+    results: [
+      "Respuesta más rápida a incidentes.",
+      "Visibilidad completa de la infraestructura.",
+      "Monitoreo 24/7."
+    ]
+  },
+  {
     slug: "SAACG.NET",
 
     title: "Sistema Financiero Gubernamental",
@@ -185,6 +227,170 @@ export const projects = [
       "Networking"
     ],
 
+    responsibilities: [
+      "Configuración de hosts.",
+      "Alertas.",
+      "Dashboards.",
+      "Monitoreo de servicios.",
+      "Análisis de disponibilidad."
+    ],
+
+    results: [
+      "Respuesta más rápida a incidentes.",
+      "Visibilidad completa de la infraestructura.",
+      "Monitoreo 24/7."
+    ]
+  },
+  {
+    slug: "VPN",
+
+    title: "VPN Empresarial",
+
+    category: "Infraestructura",
+
+    status: "Producción",
+
+    year: "2020 - Actualidad",
+
+    icon: "waypoints",
+
+    description:
+      "Implementación y administración de una red privada virtual (VPN) para permitir el acceso seguro a los recursos de la institución desde ubicaciones remotas.",
+
+    overview:
+      "Implementación y administración del sistema de red privada virtual (VPN) para permitir el acceso seguro a los recursos de la institución desde ubicaciones remotas.",
+
+    technologies: [
+      "VPN",
+      "DNS",
+      "DMZ"
+    ],
+
+    responsibilities: [
+      "Configuración de hosts.",
+      "Alertas.",
+      "Dashboards.",
+      "Monitoreo de servicios.",
+      "Análisis de disponibilidad."
+    ],
+
+    results: [
+      "Respuesta más rápida a incidentes.",
+      "Visibilidad completa de la infraestructura.",
+      "Monitoreo 24/7."
+    ]
+  },
+  {
+    slug: "Programación",
+
+    title: "Programación",
+
+    category: "Desarrollo",
+
+    status: "Test",
+
+    year: "2016 - Actualidad",
+
+    icon: "terminal",
+
+    description:
+      "Desarrollo de aplicaciones y scripts para automatizar tareas, mejorar procesos y optimizar la administración de sistemas e infraestructura tecnológica.",
+
+    overview:
+      "Desarrollo de aplicaciones y scripts para automatizar tareas, mejorar procesos y optimizar la administración de sistemas e infraestructura tecnológica.",
+
+    technologies: [
+      "Python",
+      "JavaScript",
+      "SQL",
+      "Bash",
+      "PowerShell",
+      "PHP",
+      "HTML",
+      "CSS",
+      "TypeScript"
+    ],
+    responsibilities: [
+      "Configuración de hosts.",
+      "Alertas.",
+      "Dashboards.",
+      "Monitoreo de servicios.",
+      "Análisis de disponibilidad."
+    ],
+
+    results: [
+      "Respuesta más rápida a incidentes.",
+      "Visibilidad completa de la infraestructura.",
+      "Monitoreo 24/7."
+    ]
+  },
+  {
+    slug: "Servidores en la nube",
+
+    title: "Servidores en la nube",
+
+    category: "Infraestructura",
+
+    status: "Producción",
+
+    year: "2023 - Actualidad",
+
+    icon: "cloud ",
+
+    description:
+      "Configuración y administración de servidores en la nube para garantizar disponibilidad, escalabilidad y seguridad de los servicios institucionales.",
+    overview:
+      "Configuración y administración de servidores en la nube para garantizar disponibilidad, escalabilidad y seguridad de los servicios institucionales.",
+    technologies: [
+        "aws",
+        "azure",
+        "google cloud",
+        "linux",
+        "windows server",
+        "versel",
+         ],
+    responsibilities: [
+      "Configuración de hosts.",
+      "Alertas.",
+      "Dashboards.",
+      "Monitoreo de servicios.",
+      "Análisis de disponibilidad."
+    ],
+
+    results: [
+      "Respuesta más rápida a incidentes.",
+      "Visibilidad completa de la infraestructura.",
+      "Monitoreo 24/7."
+    ]
+  },
+   {
+    slug: "Camaras de Seguridad",
+
+    title: "Camaras de Seguridad",
+
+    category: "Infraestructura",
+
+    status: "Producción",
+
+    year: "2016 - Actualidad",
+
+    icon: "cctv",
+
+    description:
+      "Instalación y administración de sistemas de videovigilancia.",
+    overview:
+      "Instalación y administración de sistemas de videovigilancia.",
+    technologies: [
+      "CCTV",
+      "Video Management Software",
+      "Video Analytics",
+      "Video Storage Solutions",
+      "IP Cameras",
+      "(NVRs)",
+      "(DVRs)",
+      "Video Monitoring",
+      "Remote Access and Control"
+    ],
     responsibilities: [
       "Configuración de hosts.",
       "Alertas.",
